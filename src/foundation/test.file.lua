30bb1b87190bc5ac07fe46ecd1a74f058f92911f
Matt Boney <matt@cloverbasin.com>
2013-07-29 19:53:52 -0700

    small test file update

--- src/foundation/test.file.lua
+++ src/foundation/test.file.lua
@@ -1,6 +1,18 @@
-local foundation = assert(require("poco"))
-local ff = assert(foundation.File("test1"))
+local poco = assert(require("poco"))
+local ff = assert(poco.File("test1"))
 
-print("file: ", ff.path)
-print("exists: ", ff.exists)
-print("size: ", ff:size)
+local fileType
+if ff:isFile() then
+	fileType = "file"
+elseif ff:isDirectory() then
+	fileType = "directory"
+elseif ff:isDevice() then
+	fileType = "device"
+else
+	fileType = "none"
+end
+
+print("file: ", ff:path())
+print("exists: ", ff:exists())
+print("type: ", fileType)
+print("size: ", ff:size())
