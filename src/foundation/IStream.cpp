875fc8093e9653709745e07bfdd1140300b9c082
Matt Boney <matt@cloverbasin.com>
2014-10-27 12:55:51 -0600

    port IStream to base Userdata mechanism.

--- src/foundation/IStream.cpp
+++ src/foundation/IStream.cpp
@@ -7,35 +7,12 @@
 namespace LuaPoco
 {
 
-static IStream* checkIStream(lua_State* L)
-{
-    IStream* isud = NULL;
-    luaL_checktype(L, 1, LUA_TUSERDATA);
-    // check for metatable attached to userdata
-    if (lua_getmetatable(L, 1))
-    {
-        // check if it is 
-        lua_getfield(L, -1, "poco.userdata");
-        if (lua_isnil(L, -1))
-            luaL_error(L, "userdata is not a poco userdata.");
-        lua_pop(L, 2);
-        // broken due to casting from void* to a base class.
-        // Userdata* ud = reinterpret_cast<Userdata*>(lua_touserdata(L, 1));
-        // if (ud->getBaseType() != BaseType_IStream)
-        //     luaL_error(L, "userdata is not an istream userdata.");
-        // isud = reinterpret_cast<IStream*>(ud);
-    }
-    
-    return isud;
-}
-
 // userdata methods
 int IStream::read(lua_State* L)
 {
     int rv = 0;
-    // this is broken due to casting from void* via Lua to base class type.
-    // IStream* isud = checkIStream(L);
-    // std::istream& is = isud->istream();
+    IStream* isud = checkPrivateUserdata<IStream>(L, 1);
+    std::istream& is = isud->istream();
     
     try
     {
@@ -56,9 +33,8 @@ int IStream::read(lua_State* L)
 int IStream::lines(lua_State* L)
 {
     int rv = 0;
-    // this is broken due to casting from void* via Lua to base class type.
-    // IStream* isud = checkIStream(L);
-    // std::istream& is = isud->istream();
+    IStream* isud = checkPrivateUserdata<IStream>(L, 1);
+    std::istream& is = isud->istream();
     
     try
     {
@@ -79,9 +55,8 @@ int IStream::lines(lua_State* L)
 int IStream::seek(lua_State* L)
 {
     int rv = 0;
-    // this is broken due to casting from void* via Lua to base class type.
-    // IStream* isud = checkIStream(L);
-    // std::istream& is = isud->istream();
+    IStream* isud = checkPrivateUserdata<IStream>(L, 1);
+    std::istream& is = isud->istream();
     
     try
     {
@@ -102,9 +77,8 @@ int IStream::seek(lua_State* L)
 int IStream::setvbuf(lua_State* L)
 {
     int rv = 0;
-    // this is broken due to casting from void* via Lua to base class type.
-    // IStream* isud = checkIStream(L);
-    // std::istream& is = isud->istream();
+    IStream* isud = checkPrivateUserdata<IStream>(L, 1);
+    std::istream& is = isud->istream();
     
     try
     {
