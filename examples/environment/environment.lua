04b8211b483d4a57054e9e365c26d3f172fabe56
Matt Boney <matt@cloverbasin.com>
2014-10-30 21:10:41 -0600

    add poco.environment example.

---
+++ examples/environment/environment.lua
@@ -0,0 +1,28 @@
+-- load the environment module from the poco library.
+local environment = assert(require("poco.environment"))
+
+-- print out environment attributes
+print("\npoco.environment functions:\n")
+print("libraryVersion:\t", environment.libraryVersion())
+print("nodeId:        \t", environment.nodeId())
+print("nodeName:      \t", environment.nodeName())
+print("osArchitecture:\t", environment.osArchitecture())
+print("osDisplayName: \t", environment.osDisplayName())
+print("osName:        \t", environment.osName())
+print("osVerison:     \t", environment.osVersion())
+
+print("\nchecking environment variables...\n")
+print("has TEMP:      \t", environment.has("TEMP"))
+if (environment.has("TEMP")) then
+    print(environment.get("TEMP"))
+end
+
+print("has HOME:      \t", environment.has("HOME"))
+if (environment.has("HOME")) then
+    print(environment.get("HOME"))
+end
+
+print("has USERPROFILE:", environment.has("USERPROFILE"))
+if (environment.has("USERPROFILE")) then
+    print(environment.get("USERPROFILE"))
+end
