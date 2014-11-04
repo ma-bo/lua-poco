fa1f724dfa156cbfee62b37804ebdc2bacdfde88
Matt Boney <matt@cloverbasin.com>
2014-11-04 15:16:25 -0700

    refactor/clean-up IStream.

--- src/foundation/IStream.cpp
+++ src/foundation/IStream.cpp
@@ -10,11 +10,195 @@ namespace LuaPoco
 
 const char* POCO_ISTREAM_METATABLE_NAME = "Poco.IStream.metatable";
 
+static int readAmount(lua_State* L, std::istream& is, size_t toRead)
+{
+    int rv = 0;
+    
+    if (toRead == 0)
+    {
+        if (is.good())
+        {
+            lua_pushstring(L, "");
+            rv = 1;
+        }
+        else if (is.eof())
+        {
+            lua_pushnil(L);
+            lua_pushstring(L, "eof");
+            rv = 2;
+        }
+        else
+        {
+            lua_pushnil(L);
+            lua_pushstring(L, "error");
+            rv = 2;
+        }
+        
+        return rv;
+    }
+    
+    char buffer[1024];
+    size_t total = 0;
+    luaL_Buffer lb;
+    luaL_buffinit(L, &lb);
+    
+    while (toRead > 0)
+    {
+        // read requested amount if it will fit in the buffer,
+        // otherwise, read the size of the buffer instead.
+        size_t amount = toRead < sizeof buffer ? toRead : sizeof buffer;
+        
+        if (is.read(buffer, amount))
+        {
+            luaL_addlstring(&lb, buffer, amount);
+            total += amount;
+            toRead -= amount;
+        }
+        else
+            break;
+    }
+    
+    // have read all requested bytes
+    if (toRead == 0)
+    {
+        luaL_pushresult(&lb);
+        rv = 1;
+    }
+    else
+    {
+        // check for partial read
+        size_t lastAmountRead = static_cast<size_t>(is.gcount());
+        if (lastAmountRead > 0) luaL_addlstring(&lb, buffer, lastAmountRead);
+        
+        total += lastAmountRead;
+        luaL_pushresult(&lb);
+        
+        // have some data, add it to the buffer and return it
+        if (total > 0)
+        {
+            rv = 1;
+        }
+        // have no data for this read request, return: nil, emsg
+        else
+        {
+            // pop finalized luaL_Buffer.
+            lua_pop(L, 1);
+            
+            lua_pushnil(L);
+            const char* emsg = is.eof() ? "eof" : "error";
+            lua_pushstring(L, emsg);
+            rv = 2;
+        }
+    }
+    
+    return rv;
+}
+
+static int readLine(lua_State* L, std::istream& is)
+{
+    int rv = 0;
+    
+    std::string line;
+    if (std::getline(is, line))
+    {
+        lua_pushlstring(L, line.c_str(), line.size());
+        rv = 1;
+    }
+    else if (is.eof())
+    {
+        lua_pushnil(L);
+        lua_pushstring(L, "eof");
+        rv = 2;
+    }
+    else
+    {
+        lua_pushnil(L);
+        lua_pushstring(L, "error");
+        rv = 2;
+    }
+    
+    return rv;
+}
+
+static int readAll(lua_State* L, std::istream& is)
+{
+    int rv = 0;
+    char buffer[1024];
+    size_t total = 0;
+    luaL_Buffer lb;
+    luaL_buffinit(L, &lb);
+    
+    while (is.read(buffer, sizeof buffer))
+    {
+        luaL_addlstring(&lb, buffer, sizeof buffer);
+        total += sizeof buffer;
+    }
+    
+    if (is.eof())
+    {
+        size_t lastReadAmount = static_cast<size_t>(is.gcount());
+        if (lastReadAmount > 0) luaL_addlstring(&lb, buffer, lastReadAmount);
+        
+        total += lastReadAmount;
+        luaL_pushresult(&lb);
+        
+        if (total > 0)
+            rv = 1;
+        else
+        {
+            // pop finalized luaL_Buffer.
+            lua_pop(L, 1);
+            lua_pushnil(L);
+            lua_pushstring(L, "eof");
+            rv = 2;
+        }
+    }
+    else
+    {
+        // finalize the luaL_Buffer and pop it off.
+        luaL_pushresult(&lb);
+        lua_pop(L, 1);
+        
+        lua_pushnil(L);
+        lua_pushstring(L, "error");
+        rv = 2;
+    }
+    
+    return rv;
+}
+
+static int readNumber(lua_State* L, std::istream& is)
+{
+    int rv = 0;
+    
+    lua_Number num;
+    if (is >> num)
+    {
+        lua_pushnumber(L, num);
+        rv = 1;
+    }
+    else if (is.eof())
+    {
+        lua_pushnil(L);
+        lua_pushstring(L, "eof");
+        rv = 2;
+    }
+    else
+    {
+        lua_pushnil(L);
+        lua_pushstring(L, "error");
+        rv = 2;
+    }
+    
+    return rv;
+}
+
 // userdata methods
 
 /// Reads from the istream.
 // This function behaves identically to Lua file:read() function.
-// @string[opt] format "*l" to read a line (default), "*n" to read a number, "*a" to read until end of file.
+// @string[opt] format "*l" to read a line (default), "*n" to read a number, 
+// "*a" to read until end of file, or a number to read the specified number of bytes.
 // @return number/string or nil. (eof/error)
 // @return error
 // @function read
@@ -23,92 +207,28 @@ int IStream::read(lua_State* L)
     int rv = 0;
     IStream* isud = checkPrivateUserdata<IStream>(L, 1);
     std::istream& is = isud->istream();
-    const char* mode = "*l";
-    
-    if (lua_isstring(L, 2))
-        mode = lua_tostring(L, 2);
-    
+
     try
     {
-        if (std::strcmp(mode, "*n") == 0)
+        // check for an amount of bytes to read.
+        if (lua_isnumber(L, 2)) rv = readAmount(L, is, lua_tonumber(L, 2));
+        // otherwise a read mode has been specified.
+        else if (lua_isstring(L, 2))
         {
-            lua_Number num;
-            if (is >> num)
-            {
-                lua_pushnumber(L, num);
-                rv = 1;
-            }
-            else if (is.eof())
-            {
-                lua_pushnil(L);
-                lua_pushstring(L, "eof");
-                rv = 2;
-            }
-            else
-            {
-                lua_pushnil(L);
-                lua_pushstring(L, "error");
-                rv = 2;
-            }
-        }
-        else if (std::strcmp(mode, "*a") == 0)
-        {
-            char buffer[1024];
-            size_t got = 0;
-            size_t total = 0;
-            luaL_Buffer lb;
-            luaL_buffinit(L, &lb);
+            const char* mode = lua_tostring(L, 2);
             
-            while (is.read(buffer, sizeof buffer))
-            {
-                luaL_addlstring(&lb, buffer, sizeof buffer);
-                total += sizeof buffer;
-            }
-            
-            if (is.eof())
-            {
-                total += static_cast<size_t>(is.gcount());
-                luaL_addlstring(&lb, buffer, static_cast<size_t>(is.gcount()));
-                luaL_pushresult(&lb);
-                if (total > 0)
-                    rv = 1;
-                else
-                {
-                    lua_pushnil(L);
-                    lua_pushstring(L, "eof");
-                    rv = 2;
-                }
-            }
-            else
-            {
-                luaL_pushresult(&lb);
-                lua_pop(L, 1);
-                lua_pushnil(L);
-                lua_pushstring(L, "error");
-                rv = 2;
-            }
-        }
-        else
-        {
-            std::string line;
-            if (std::getline(is, line))
-            {
-                lua_pushlstring(L, line.c_str(), line.size());
-                rv = 1;
-            }
-            else if (is.eof())
-            {
-                lua_pushnil(L);
-                lua_pushstring(L, "eof");
-                rv = 2;
-            }
+            if (std::strcmp(mode, "*n") == 0) rv = readNumber(L, is);
+            else if (std::strcmp(mode, "*a") == 0) rv = readAll(L, is);
+            else if (std::strcmp(mode, "*l") == 0) rv = readLine(L, is);
             else
             {
                 lua_pushnil(L);
-                lua_pushstring(L, "error");
+                lua_pushstring(L, "invalid mode");
                 rv = 2;
             }
         }
+        // no mode specified, read a line as per the default for file:read().
+        else rv = readLine(L, is);
     }
     catch (const Poco::Exception& e)
     {
@@ -148,25 +268,7 @@ int IStream::line(lua_State* L)
     
     try
     {
-        std::string line;
-        std::getline(is, line);
-        if (is.good())
-        {
-            lua_pushlstring(L, line.c_str(), line.size());
-            rv = 1;
-        }
-        else if (is.eof())
-        {
-            lua_pushnil(L);
-            lua_pushstring(L, "eof");
-            rv = 2;
-        }
-        else
-        {
-            lua_pushnil(L);
-            lua_pushstring(L, "error");
-            rv = 2;
-        }
+        rv = readLine(L, is);
     }
     catch (const Poco::Exception& e)
     {
