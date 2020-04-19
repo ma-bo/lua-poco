daad0b0dc09efe9cf72e38851877d1474b72c7e1
ma-bo <matt@cloverbasin.com>
2020-04-19 14:49:06 -0600

    add some commentary to LuaPocoUtils.h and compress code slightly.

--- src/LuaPocoUtils.h
+++ src/LuaPocoUtils.h
@@ -1,4 +1,5 @@
 #include <limits>
+#include <type_traits>
 #include "LuaPoco.h"
 
 namespace LuaPoco
@@ -7,21 +8,22 @@ namespace LuaPoco
 template <typename T>
 bool checkUnsignedToLuaInteger(T num, lua_Integer& li)
 {
+    static_assert(std::numeric_limits<T>::radix == 2,
+                  "checkUnsignedToLuaInteger requires binary representation for source type.");
+    
     if (num <= std::numeric_limits<lua_Integer>::max())
     {
         li = static_cast<lua_Integer>(num);
-        // corollary to Javascript's MAX_SAFE_INTEGER.
-        // Lua 5.1, and 5.2 use doubles to store numbers, make sure this lua_Integer will fit.
         #if LUA_VERSION_NUM < 503
-        const int delta = std::numeric_limits<lua_Integer>::digits - std::numeric_limits<lua_Number>::digits;
+        // corollary to Javascript's MAX_SAFE_INTEGER.
+        // As Lua 5.1, and 5.2 use doubles to store numbers, make sure this lua_Integer can be
+        // stored without losing precision.
         
-        if (delta > 0)
-        {
-            if (num < (std::numeric_limits<T>::max() >> delta))
-            {
-                return true;
-            }
-        }
+        // if the lua_Integer type has more bits than lua_Number, calculate the delta in bits, and
+        // shift the maximum lua_Integer value down to match the number of bits available in the
+        // lua_Number, and see if the current value is within range.
+        const int delta = std::numeric_limits<lua_Integer>::digits - std::numeric_limits<lua_Number>::digits;
+        if (delta > 0 && (num < (std::numeric_limits<T>::max() >> delta))) { return true; }
         #else
         return true;
         #endif
@@ -33,24 +35,17 @@ bool checkUnsignedToLuaInteger(T num, lua_Integer& li)
 template <typename T>
 bool checkSignedToLuaInteger(T num, lua_Integer& li)
 {
+    static_assert(std::numeric_limits<T>::radix == 2,
+                  "checkSignedToLuaInteger requires binary representation for source type.");
     
     if (num > 0) return checkUnsignedToLuaInteger<T>(num, li);
-    
-    if (num >= std::numeric_limits<lua_Integer>::min())
+    else if (num >= std::numeric_limits<lua_Integer>::min())
     {
         li = static_cast<lua_Integer>(num);
-        // corollary to Javascript's MIN_SAFE_INTEGER.
-        // Lua 5.1, and 5.2 use doubles to store numbers, make sure this lua_Integer will fit.
         #if LUA_VERSION_NUM < 503
+        // corollary to Javascript's MIN_SAFE_INTEGER, see decription for checkUnsignedToLuaInteger.
         const int delta = std::numeric_limits<lua_Integer>::digits - std::numeric_limits<lua_Number>::digits;
-        
-        if (delta > 0)
-        {
-            if (num > (std::numeric_limits<T>::min() >> delta))
-            {
-                return true;
-            }
-        }
+        if (delta > 0 && (num > (std::numeric_limits<T>::min() >> delta))) { return true; }
         #else
         return true;
         #endif
